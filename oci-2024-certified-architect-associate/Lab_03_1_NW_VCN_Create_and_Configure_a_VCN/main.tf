eee8cb666006d448aeb2416eda7b5f1b0853c5c3
Douglas Hawthorne <dfhawthorne@computer.org>
2024-09-01 04:27:01 +1000

    Completed CAA Lab 03

--- oci-2024-certified-architect-associate/Lab_03_1_NW_VCN_Create_and_Configure_a_VCN/main.tf
+++ oci-2024-certified-architect-associate/Lab_03_1_NW_VCN_Create_and_Configure_a_VCN/main.tf
@@ -45,8 +45,8 @@ resource "oci_core_internet_gateway" "Internet-gateway-FRA-AA-LAB03-VCN-01" {
   vcn_id            = oci_core_vcn.FRA-AA-LAB03-VCN-01.id
 }
 
-resource "oci_core_dhcp_options" "DHCP-Options-for-FRA-AA-LAB03-VCN-01" {
-  vcn_id            = oci_core_vcn.FRA-AA-LAB03-VCN-01.id
+resource "oci_core_default_dhcp_options" "DHCP-Options-for-FRA-AA-LAB03-VCN-01" {
+  manage_default_resource_id = oci_core_vcn.FRA-AA-LAB03-VCN-01.default_dhcp_options_id
   compartment_id    = var.compartment_id
   display_name      = "DHCP Options for FRA-AA-LAB03-VCN-01"
   domain_name_type  = "CUSTOM_DOMAIN"
@@ -67,14 +67,12 @@ resource "oci_core_dhcp_options" "DHCP-Options-for-FRA-AA-LAB03-VCN-01" {
 resource "oci_core_subnet" "public-subnet-FRA-AA-LAB03-VCN-01" {
   cidr_block     = "10.0.0.0/24"
   compartment_id = var.compartment_id
-  dhcp_options_id = oci_core_dhcp_options.DHCP-Options-for-FRA-AA-LAB03-VCN-01.id
   display_name    = "public subnet-FRA-AA-LAB03-VCN-01"
   dns_label       = "sub07051559030"
   ipv6cidr_blocks = [
   ]
   prohibit_internet_ingress  = "false"
   prohibit_public_ip_on_vnic = "false"
-  route_table_id             = oci_core_route_table.default-route-table-for-FRA-AA-LAB03-VCN-01.id
   security_list_ids = [
     oci_core_vcn.FRA-AA-LAB03-VCN-01.default_security_list_id,
   ]
@@ -84,7 +82,6 @@ resource "oci_core_subnet" "public-subnet-FRA-AA-LAB03-VCN-01" {
 resource oci_core_subnet private-subnet-FRA-AA-LAB03-VCN-01 {
   cidr_block     = "10.0.1.0/24"
   compartment_id = var.compartment_id
-  dhcp_options_id = oci_core_dhcp_options.DHCP-Options-for-FRA-AA-LAB03-VCN-01.id
   display_name    = "private subnet-FRA-AA-LAB03-VCN-01"
   dns_label       = "sub07051559031"
   ipv6cidr_blocks = [
@@ -107,22 +104,22 @@ resource oci_core_route_table route-table-for-private-subnet-FRA-AA-LAB03-VCN-01
     network_entity_id = oci_core_nat_gateway.NAT-gateway-FRA-AA-LAB03-VCN-01.id
   }
   route_rules {
-    destination       = "all-iad-services-in-oracle-services-network"
+    destination       = "all-fra-services-in-oracle-services-network"
     destination_type  = "SERVICE_CIDR_BLOCK"
     network_entity_id = oci_core_service_gateway.Service-gateway-FRA-AA-LAB03-VCN-01.id
   }
   vcn_id = oci_core_vcn.FRA-AA-LAB03-VCN-01.id
 }
 
-resource oci_core_route_table default-route-table-for-FRA-AA-LAB03-VCN-01 {
-  compartment_id = var.compartment_id
-  display_name = "default route table for FRA-AA-LAB03-VCN-01"
+resource oci_core_default_route_table default-route-table-for-FRA-AA-LAB03-VCN-01 {
+  compartment_id              = var.compartment_id
+  manage_default_resource_id  = oci_core_vcn.FRA-AA-LAB03-VCN-01.default_route_table_id
+  display_name                = "Default Route Table for FRA-AA-LAB03-VCN-01"
   route_rules {
     destination       = "0.0.0.0/0"
     destination_type  = "CIDR_BLOCK"
     network_entity_id = oci_core_internet_gateway.Internet-gateway-FRA-AA-LAB03-VCN-01.id
   }
-  vcn_id = oci_core_vcn.FRA-AA-LAB03-VCN-01.id
 }
 
 
